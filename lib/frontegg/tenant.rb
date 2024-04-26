e06343214b8f07ed5108a628edd11736a2a40849
MaicolBen <maicol.bentancor@gmail.com>
2024-04-26 13:12:05 -0300

    Add isReseller and list to tenant

--- lib/frontegg/tenant.rb
+++ lib/frontegg/tenant.rb
@@ -2,13 +2,13 @@ module Frontegg
   class Tenant < Resource
     self.base_path = '/tenants/resources/tenants/v1'
 
-    def create(name:, website: nil, logo_url: nil, metadata: {})
-      body = { tenantId: resource_id, name:, website:, logoUrl: logo_url, metadata: metadata.to_json }
+    def create(name:, website: nil, logo_url: nil, is_reseller: false, metadata: {})
+      body = { tenantId: resource_id, name:, website:, logoUrl: logo_url, isReseller: is_reseller, metadata: metadata.to_json }
       client.execute_request(:post, path, body:)
     end
 
-    def update(name:, website: nil, logo_url: nil, metadata: {})
-      body = { name:, website:, logoUrl: logo_url, metadata: metadata.to_json }
+    def update(name:, website: nil, logo_url: nil, is_reseller: false, metadata: {})
+      body = { name:, website:, logoUrl: logo_url, isReseller: is_reseller, metadata: metadata.to_json }
       client.execute_request(:put, resource_url, body:)
     end
 
@@ -18,6 +18,11 @@ module Frontegg
 
     def delete
       client.execute_request(:delete, resource_url)
+    end
+
+    def list(filter: nil, limit: nil)
+			path = "/tenants/resources/tenants/v2"
+      client.execute_request(:get, path, params: { _filter: filter, _limit: limit } )
     end
   end
 end
